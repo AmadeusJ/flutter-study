8ebaf2742c78ba849bb805d9700e97132704231c
Amadeus J <dawoonjung1026@gmail.com>
2025-02-18 18:02:12 +0900

    flutter first study

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'styled_text.dart';
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 26, 26),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(text: 'Hello, World!'),
+      ),
+    );
+  }
+}
